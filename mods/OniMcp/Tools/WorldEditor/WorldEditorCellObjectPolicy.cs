2ea9600c2071c90c2e8d9e8c28b1368f29b18726
LIghtJUNction <lightjunction.me@gmail.com>
2026-09-18 00:21:27 +0800

    refactor(onimcp): isolate world-editor building selection

---
+++ mods/OniMcp/Tools/WorldEditor/WorldEditorCellObjectPolicy.cs
@@ -0,0 +1,11 @@
+namespace OniMcp.Tools
+{
+    internal static class WorldEditorCellObjectPolicy
+    {
+        internal static T SelectBuildingCandidate<T>(T building, T logicGate, T gantry)
+            where T : class
+        {
+            return building ?? logicGate;
+        }
+    }
+}
