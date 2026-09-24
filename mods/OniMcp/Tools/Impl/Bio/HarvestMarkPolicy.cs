5433b257122c39fc95ffb15d962dac01b60f5611
LIghtJUNction <lightjunction.me@gmail.com>
2026-09-24 22:24:37 +0800

    test(onimcp): expose harvest mark policy

---
+++ mods/OniMcp/Tools/Impl/Bio/HarvestMarkPolicy.cs
@@ -0,0 +1,10 @@
+namespace OniMcp.Tools
+{
+    public static class HarvestMarkPolicy
+    {
+        public static bool ShouldMarkNow(bool canBeHarvested, bool readyOnly)
+        {
+            return !readyOnly || canBeHarvested;
+        }
+    }
+}
